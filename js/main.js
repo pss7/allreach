5e64e9842ef0dd4eb99e0db934dac58115d0e689
parksangseok <park0157@naver.com>
2025-09-23 14:04:58 +0900

    commit

--- js/main.js
+++ js/main.js
@@ -26,26 +26,44 @@ $(function () {
     $(this).parents('.brandList').removeClass('pause');
   });
 
+  //올리치어 전당 슬라이드 
+  $("#templeWrap .slick").slick({
+    autoplay: false,
+    arrows: true,
+    dots: true,
+    accessibility: false,
+    draggable: true,
+    infinite: false,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    zIndex: 1000,
+    pauseOnHover: false,
+    autoplaySpeed: 700,
+    speed: 1000,
+    prevArrow: $('#templeWrap .controlBox .prev'),
+    nextArrow: $('#templeWrap .controlBox .next'),
+    responsive: [
+      {
+        breakpoint: 1025,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+
+  });
 
 
-  // $("#mainSlideWrap .slick").slick({
-  //   autoplay: false,
-  //   arrows: true,
-  //   dots: false,
-  //   accessibility: false,
-  //   draggable: true,
-  //   infinite: true,
-  //   slidesToShow: 3,
-  //   slidesToScroll: 1,
-  //   zIndex: 1000,
-  //   pauseOnHover: false,
-  //   autoplaySpeed: 700,
-  //   speed: 2000,
-  //   centerPadding: '0px',
-  //   centerMode: false,
-  //   prevArrow: $('#mainSlideWrap .controlBox .prev'),
-  //   nextArrow: $('#mainSlideWrap .controlBox .next'),
-  // });
+
+
 
 
 });
