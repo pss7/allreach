2feba25e75a21d40d309068805ff9276f88536ea
parksangseok <park0157@naver.com>
2025-09-23 16:47:10 +0900

    commit

--- js/main.js
+++ js/main.js
@@ -61,54 +61,7 @@ $(function () {
 
   });
 
-  function initComLists() {
-
-    $('.comSlideList').each(function () {
-
-      const $wrap = $(this);
-      const $slick = $wrap.find('.slick');
-      const $prev = $wrap.find('.controlBox .prev');
-      const $next = $wrap.find('.controlBox .next');
-
-      if ($slick.hasClass('slick-initialized')) return;
-
-      $slick.slick({
-
-        variableWidth: true,
-        autoplay: false,
-        arrows: true,
-        dots: false,
-        accessibility: false,
-        draggable: true,
-        infinite: true,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        zIndex: 1000,
-        pauseOnHover: false,
-        autoplaySpeed: 700,
-        speed: 1000,
-
-        prevArrow: $prev,
-        nextArrow: $next,
-
-        // 반응형
-        responsive: [
-          {
-            breakpoint: 768,
-            settings: {
-              variableWidth: false,
-              slidesToShow: 2,
-              slidesToScroll: 1
-            }
-          }
-        ]
-      });
-    });
-
-  }
-
-  initComLists();
-
+ 
 
 });
 
