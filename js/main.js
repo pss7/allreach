0c95b7330339d6338e3a5701635e98952347c8c0
parksangseok <park0157@naver.com>
2025-09-23 15:51:20 +0900

    commit

--- js/main.js
+++ js/main.js
@@ -61,25 +61,53 @@ $(function () {
 
   });
 
-  $(".comList.slick").slick({
-    autoplay: false,
-    arrows: true,
-    dots: true,
-    accessibility: false,
-    draggable: true,
-    infinite: false,
-    slidesToShow: 5,
-    slidesToScroll: 4,
-    zIndex: 1000,
-    pauseOnHover: false,
-    autoplaySpeed: 700,
-    speed: 1000,
-    prevArrow: $('#templeWrap .controlBox .prev'),
-    nextArrow: $('#templeWrap .controlBox .next'),
-  });
+  function initComLists() {
 
+    $('.comSlideList').each(function () {
 
+      const $wrap = $(this);
+      const $slick = $wrap.find('.slick');
+      const $prev = $wrap.find('.controlBox .prev');
+      const $next = $wrap.find('.controlBox .next');
 
+      if ($slick.hasClass('slick-initialized')) return;
+
+      $slick.slick({
+
+        variableWidth: true,
+        autoplay: false,
+        arrows: true,
+        dots: false,
+        accessibility: false,
+        draggable: true,
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        zIndex: 1000,
+        pauseOnHover: false,
+        autoplaySpeed: 700,
+        speed: 1000,
+
+        prevArrow: $prev,
+        nextArrow: $next,
+
+        // 반응형
+        responsive: [
+          {
+            breakpoint: 768,
+            settings: {
+              variableWidth: false,
+              slidesToShow: 2,
+              slidesToScroll: 1
+            }
+          }
+        ]
+      });
+    });
+
+  }
+
+  initComLists();
 
 
 });
