3839088d5aa1989196a7f2a91cd35b1a7a2649a0
parksangseok <park0157@naver.com>
2025-09-24 16:45:53 +0900

    commit

--- js/sub.js
+++ js/sub.js
@@ -10,6 +10,16 @@ $(function () {
     }
   });
 
+  //선택아코디언
+  $('.customSelectBox .btn').click(function () {
+    if ($(this).next().hasClass('active')) {
+      $('.customSelectBox .btn').next().removeClass('active');
+    } else {
+      $('.customSelectBox .btn').next().removeClass('active');
+      $(this).next().addClass('active');
+    }
+  });
+
 
 });
 
